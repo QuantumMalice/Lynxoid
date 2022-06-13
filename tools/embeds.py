3affa5dfd165a183ef7da275b4378da3645e0dd6
QuantumMalice <92159895+QuantumMalice@users.noreply.github.com>
2022-06-12 20:46:21 -0600

    Create embeds.py

---
+++ tools/embeds.py
@@ -0,0 +1,26 @@
+from nextcord.ext import commands
+from nextcord import Embed, Colour, User
+
+class EmbedTool(commands.Cog):
+    
+    async def prepare_embed(
+                            user: User=None,
+                            title: str=None,
+                            description: str=None,
+                            colour: Colour=Colour.green(),
+                            ) -> Embed:
+        if title == None:
+            title = ""
+        elif description == None:
+            description = ""
+
+        embed = Embed(
+            title = title,
+            description = description,
+            colour = colour
+        )
+
+        if not user == None:
+            embed.set_author(name=user, icon_url=user.avatar)
+
+        return embed
