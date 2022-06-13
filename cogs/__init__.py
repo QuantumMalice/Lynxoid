b748a331c47e64b2b11ab3eb44e040ea9ee7830e
QuantumMalice <92159895+QuantumMalice@users.noreply.github.com>
2022-06-12 20:56:06 -0600

    Update __init__.py

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,3 +1,4 @@
+from nextcord import Game, Status
 from nextcord.ext import commands
 
 class __Init__(commands.Cog, name="Init"):
@@ -7,6 +8,9 @@ class __Init__(commands.Cog, name="Init"):
 
     @commands.Cog.listener()
     async def on_ready(self):
+        activity = Game(name="/help", type=3)
+        await self.bot.change_presence(status=Status.online, activity=activity)
+
         print("Lynxiod is alive!")
 
 def setup(bot):
