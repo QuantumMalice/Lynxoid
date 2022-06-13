6a9dc36342d61235449dddd02f1e4016f4e0ef4c
QuantumMalice <92159895+QuantumMalice@users.noreply.github.com>
2022-06-12 20:46:03 -0600

    Update commands.py

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,9 +1,24 @@
+import os, nextcord
+from nextcord import Colour, slash_command
 from nextcord.ext import commands
+from tools.embeds import *
+
 
 class Commands(commands.Cog, name="Commands"):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+
+    # [Ping]: Bot Latency
+    @slash_command(
+        name='ping',
+        description='Return a message with bot latency', guild_ids=[])
+    async def ping(self, interaction: nextcord.Interaction):
+            bot = self.bot
+            botUser = bot.users[0]
+            ping = round(bot.latency * 1000)
+            embed = await EmbedTool.prepare_embed(botUser, None, f"> ***Ping:*** **`{ping}ms`**\n", Colour.yellow())
+            await interaction.response.send_message(embed=embed, ephemeral=True)
 
 def setup(bot):
     bot.add_cog(Commands(bot))
